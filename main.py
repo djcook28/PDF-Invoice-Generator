d4d443ce2069e1dcaf14df9e700fdaa1e9977980
dcook <daniel.j.cook.28@gmail.com>
2025-03-23 14:00:37 -0500

    refined filename parsing using pathlib library

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
 import glob
 from fpdf import FPDF
 import pandas as pd
+from pathlib import Path
 
 filepaths = glob.glob('Excel Invoices/*.xlsx')
 
 for filepath in filepaths:
     invoice_df = pd.read_excel(filepath, sheet_name='Sheet 1')
-    print(filepath)
 
-    start_of_filename = filepath.find("\\")
-    start_of_date = filepath.find("-")
-    end_of_date = filepath.find(".xlsx")
-    invoice_number = filepath[start_of_filename+1:start_of_date]
-    invoice_date = filepath[start_of_date+1:end_of_date]
-    print((invoice_date))
+    filename = Path(filepath).stem
+    filename_split = filename.split("-")
+    invoice_number = filename_split[0]
+    invoice_date = filename_split[1]
 
     pdf = FPDF(orientation='P', unit='mm', format='A4')
 
@@ -25,4 +23,4 @@ for filepath in filepaths:
     pdf.cell(50, 12, txt=f'Invoice nr. {invoice_number}')
     pdf.cell(0, 12, txt=f'Date {invoice_date}')
 
-    pdf.output(filepath.replace('.xlsx', '.pdf'))
+    pdf.output(f"PDFs/{filename}.pdf")
