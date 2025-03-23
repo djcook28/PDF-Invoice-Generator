198d7e847bc82cc5c85e0ec50e32e0ef53a86dd6
dcook <daniel.j.cook.28@gmail.com>
2025-03-23 13:56:31 -0500

    my crude solution for parsing the filename

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-import os
+import glob
 from fpdf import FPDF
 import pandas as pd
 
-invoices = os.listdir('Excel Invoices')
+filepaths = glob.glob('Excel Invoices/*.xlsx')
 
-for invoice in invoices:
-    print(f'Excel Invoices/{invoice}')
-    invoice_df = pd.read_excel(f'Excel Invoices/{invoice}')
+for filepath in filepaths:
+    invoice_df = pd.read_excel(filepath, sheet_name='Sheet 1')
+    print(filepath)
+
+    start_of_filename = filepath.find("\\")
+    start_of_date = filepath.find("-")
+    end_of_date = filepath.find(".xlsx")
+    invoice_number = filepath[start_of_filename+1:start_of_date]
+    invoice_date = filepath[start_of_date+1:end_of_date]
+    print((invoice_date))
 
     pdf = FPDF(orientation='P', unit='mm', format='A4')
 
@@ -15,6 +22,7 @@ for invoice in invoices:
     pdf.set_font(family="Times", size=12)
     pdf.set_text_color(110,110,110)
 
-    pdf.cell(0, 12, txt=f'Invoice {invoice}')
+    pdf.cell(50, 12, txt=f'Invoice nr. {invoice_number}')
+    pdf.cell(0, 12, txt=f'Date {invoice_date}')
 
-    pdf.output(invoice.replace('.xlsx', '.pdf'))
+    pdf.output(filepath.replace('.xlsx', '.pdf'))
