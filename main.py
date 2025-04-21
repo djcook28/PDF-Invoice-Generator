646d6e4bcf00f0dfebdcbb93d5ef14ace9a3edc9
dcook <daniel.j.cook.28@gmail.com>
2025-04-20 21:02:51 -0500

    Added invoice total and 'company' logo

--- main.py
+++ main.py
@@ -52,4 +52,17 @@ for filepath in filepaths:
         pdf.cell(w=40, h=8, txt=str(row["price_per_unit"]), border=1)
         pdf.cell(w=40, h=8, txt=str(row["total_price"]), border=1, ln=1)
 
+    total_sum = invoice_df["total_price"].sum()
+    pdf.set_font(family="Times", size=8)
+    pdf.cell(w=40, h=8, txt="", border=1)
+    pdf.cell(w=40, h=8, txt="", border=1)
+    pdf.cell(w=40, h=8, txt="", border=1)
+    pdf.cell(w=40, h=8, txt="", border=1)
+    pdf.cell(w=40, h=8, txt=str(total_sum), border=1, ln=1)
+
+    pdf.set_font(family="Times", size=10)
+    pdf.cell(w=30, h=10, txt=f"The total invoice amount is {total_sum}", ln=1)
+    pdf.cell(w=20, h=10, txt="PythonHow")
+    pdf.image("pythonhow.png", w=10, h=10)
+
     pdf.output(f"PDFs/{filename}.pdf")
